c815fb618c02eceddfbbc6881aa54e4e8b494d3f
Paul McLanahan <pmac@mozilla.com>
2019-03-05 14:22:40 -0500

    Add cnames config and redirects

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import responder
+import toml
 
 
 api = responder.API()
+CNAMES = toml.load('cnames.toml')['cnames']
 
 
 @api.route('/', default=True)
 def hello_world(req, resp):
-    resp.media = dict(**req.headers)
+    host = req.headers.get('host')
+    if host:
+        parts = host.split('.')
+        if len(parts) == 2:
+            resp.text = 'Apex domain. Template goes here.'
+            return
+
+        if len(parts) == 3:
+            cname = parts[0]
+            if cname in CNAMES:
+                api.redirect(resp, CNAMES[cname])
+                return
+
+    resp.status_code = api.status_codes.HTTP_404
 
 
 if __name__ == '__main__':
--- test_app.py
+++ test_app.py
@@ -9,14 +9,30 @@ def api():
 
 
 def test_routes(api):
+    resp = api.requests.get("/", allow_redirects=False, headers={
+        'host': 'pmac.thepy.dev',
+    })
+    assert resp.status_code == 301
+    assert resp.headers['location'] == 'https://pmac.io/'
+
+    # apex domain is an actual page
+    resp = api.requests.get("/", allow_redirects=False, headers={
+        'host': 'thepy.dev',
+    })
+    assert resp.status_code == 200
+
+    # only one level of subdomain supported
+    resp = api.requests.get("/", allow_redirects=False, headers={
+        'host': 'too.many.thepy.dev',
+    })
+    assert resp.status_code == 404
+
+    # subdomain not in config
+    resp = api.requests.get("/", allow_redirects=False, headers={
+        'host': 'unknown-one.thepy.dev',
+    })
+    assert resp.status_code == 404
+
+    # no host
     resp = api.requests.get("/", allow_redirects=False)
-    assert resp.status_code == 301
-    assert resp.headers['location'] == 'https://pmac.io/'
-
-    resp = api.requests.get("/dude", allow_redirects=False)
-    assert resp.status_code == 301
-    assert resp.headers['location'] == 'https://pmac.io/'
-
-    resp = api.requests.get("/dude/", allow_redirects=False)
-    assert resp.status_code == 301
-    assert resp.headers['location'] == 'https://pmac.io/'
+    assert resp.status_code == 404
