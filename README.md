a8f8fd4a7956358d29c64e1ab3cb9891ff804b87
Paul McLanahan <pmac@mozilla.com>
2019-03-05 16:31:45 -0500

    Add templates and update things

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# pmac.io redirector
+# thepy.dev and thedjango.dev Redirector
 
-A redirector service for various domains to point to my primary one. They are .dev domains so https is required
-and the redirects that my registrar will do are http only 😞 
+A redirector service for my python-related .dev domains to point to other URLs. They are .dev domains so https is required.
 
 Hosted on my [Linode](https://www.linode.com/?r=d069f8df6df96b5407f86bbc2dde1424435ba75c) and deployed using [Dokku](http://dokku.viewdocs.io/dokku/).
+
+Would you like your own subdomain redirector? Just submit a PR to this repo with an update
+to the [cnames.toml](cnames.toml) file and I will merge and deploy it if it's reasonable.
--- app.py
+++ app.py
@@ -8,35 +8,42 @@ from statsd import StatsClient
 
 api = responder.API()
 statsd = StatsClient()
-CNAMES = toml.load('cnames.toml')['cnames']
+CNAMES = toml.load('cnames.toml')
 STATSD_URL = getenv('STATSD_URL')
 if STATSD_URL:
     match = re.match(r'^statsd://(\w+):(\d+)$', STATSD_URL)
     if match:
         statsd_host, statsd_port = match.groups()
-        statsd = StatsClient(host=statsd_host, port=int(statsd_port),
-                             prefix='thepy-dev')
+        statsd = StatsClient(host=statsd_host, port=int(statsd_port))
 
 
 @api.route('/', default=True)
 def hello_world(req, resp):
-    host = req.headers.get('host')
-    if host:
-        parts = host.split('.')
+    # full_host = req.headers.get('host')
+    full_host = 'thepy.dev'
+    if full_host:
+        parts = full_host.split('.')
+        host = parts[-2]
         if len(parts) == 2:
-            resp.text = 'Apex domain. Template goes here.'
-            statsd.incr('homepage')
+            resp.html = api.template('home.html', host=host)
+            statsd.incr(f'{host}.homepage')
             return
 
         if len(parts) == 3:
             cname = parts[0]
-            if cname in CNAMES:
-                api.redirect(resp, CNAMES[cname])
-                statsd.incr(f'cname.{cname}')
+            if host in CNAMES and cname in CNAMES[host]:
+                api.redirect(resp, CNAMES[host][cname])
+                statsd.incr(f'{host}.cname.{cname}')
                 return
 
+            statsd.incr(f'{host}.cname.{cname}.404')
+
+    else:
+        host = 'NONE'
+
     resp.status_code = api.status_codes.HTTP_404
-    statsd.incr('404')
+    resp.html = api.template('404.html')
+    statsd.incr(f'{host}.404')
 
 
 if __name__ == '__main__':
